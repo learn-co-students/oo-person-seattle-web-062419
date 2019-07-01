2bf61bea762125e16086a2d4bf7c2945b4438884
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-01 04:54:18 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,61 @@
-# your code goes here
+class Person
+
+  attr_reader :name
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+
+  end
+
+  def happiness=(happiness_index)
+    @happiness = happiness_index.clamp(0, 10)
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def hygiene=(hygiene_index)
+    @hygiene = hygiene_index.clamp(0, 10)
+  end
+
+  def hygiene
+    @hygiene
+  end
+
+  def happy?
+    if @happiness > 7
+      true
+    else
+      false
+    end
+  end
+
+  def clean?
+    if @hygiene > 7
+      true
+    else
+      false
+    end
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+end
